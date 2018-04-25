b9229fcb75e6a85cdfd4c790f56c721fc0d42246
strale-bandzi <bandzi.fearless@gmail.com>
2018-04-25 13:36:22 +0200

    bootstrap 4 theme

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+$(function() {
+  // Auto play modal video
+  $(".video").click(function () {
+    var theModal = $(this).data("target"),
+    videoSRC = $(this).attr("data-video"),
+    videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+    $(theModal + ' iframe').attr('src', videoSRCauto);
+    $(theModal + ' button.close').click(function () {
+      $(theModal + ' iframe').attr('src', videoSRC);
+    });
+  });
+});
+
+
+//Lightbox
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+
+
+//Slider SLick
+
+$('.slider').slick({
+    infinite: true,
+    slideToShow: 3,
+    slideToScroll: 1
+  });
